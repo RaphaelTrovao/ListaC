778f8568b7fcd1f633b6464d7d9b3c541eed92b9
RaphaelTrovao <raphaeltrovao123@gmail.com>
2026-05-26 12:59:35 -0300

    Geração de mapa

--- Minesweeper.c
+++ Minesweeper.c
@@ -1,54 +1,96 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
-int gerarMapa(int mp, int a, int b);
+int **mapaInit(int mpz, int a);
+int gerarMapa(int mpsz, int mp[mpsz][mpsz], int a, int b);
+int mapDig(int x, int  y);
+int exibirMat(int mpsz, int **mp);
 
 void main(){
     int clickx = 3, clicky = 3;
     int mapsize = 7;
-    gerarMapa(mapsize, clickx, clicky);
+    int **mapa;
+    mapa = mapaInit(mapsize, 0);
+     exibirMat(mapsize, mapa);
+    //memset(mapa, 0, sizeof(mapa));
+    scanf("%d ", &clickx);
+    scanf("%d ", &clicky);
+    //gerarMapa(mapsize, mapa, clickx, clicky);
+    int x, y, win = 0;
+    /*while(win == 0){
+        scanf("%d", &x);
+        scanf("%d", &y);
+        mapDig(x, y);
+    }*/
 }
 
-int gerarMapa(int mp, int a, int b){
-    int mapa[mp][mp];
+int exibirMat(int mpsz, int **mp){
+    for(int e = 0; e < mpsz; e++){
+        for(int f = 0; f<mpsz; f++){
+            printf("%d ", mp[e][f]);
+        }
+        printf("\n");
+    }
+   
+}
+
+int **mapaInit(int mpz, int a){
+    int **mp;
+    mp = malloc(sizeof(int *) * mpz);
+
+    for(int i = 0; i < mpz; i++){
+        mp[i] = malloc(sizeof(int ) * mpz);
+    }
+    for(int i = 0; i < mpz; i++){
+        for(int j = 0; j < mpz; j++){
+            mp[i][j] = a+1;
+            //printf("%d ", mp[i][j]);
+        }
+        
+    }
+    return mp;
+}
+
+int gerarMapa(int mpsz, int mp[mpsz][mpsz],int a, int b){
+    printf("Gerando Mapa 0%%\n");
     int range = 1;
     srand(time(NULL));
-    for(int i = 0; i < mp; i++){
-        for(int j = 0; j < mp; j++){
-            mapa[i][j] = 0;
+    for(int i = 0; i < mpsz; i++){
+        for(int j = 0; j < mpsz; j++){
             int random = rand()%100;
             //printf("%d ", random);
             if(random < 35){
                 if(i < a-range || i > a+range || j < b-range || j > b+range){
-                mapa[i][j] = 9;
+                mp[i][j] = 9;
                 }
             }
         }
     }
-    
-    for(int i = 0; i < mp; i++){
-        for(int j = 0; j < mp; j++){
+    printf("Gerando Mapa 50%%\n");
+    for(int i = 0; i < mpsz; i++){
+        for(int j = 0; j < mpsz; j++){
             int bomba = 0;
-            if(mapa[i][j] == 0){
+            if(mp[i][j] == 0){
                 for(int x = i-1; x <= i+1; x++){
                     for(int y = j-1; y <= j+1; y++){
-                        if(x < mp && y < mp && x > -1 && y > -1 && mapa[x][y] == 9){
+                        if(x < mpsz && y < mpsz && x > -1 && y > -1 && mp[x][y] == 9){
                             bomba++;
                         }
                     }
                     
                 }
-                mapa[i][j] = bomba;
+                mp[i][j] = bomba*10;
             }
            
         }
         
     }
-    for(int e = 0; e < mp; e++){
-        for(int f = 0; f<mp; f++){
-            printf("%d ", mapa[e][f]);
-        }
-        printf("\n");
-    }
+    printf("Gerando Mapa 100%%\n");
+    //exibirMat(mpsz, mp);
+}
+
+int mapDig(int x, int  y){
+
 }
